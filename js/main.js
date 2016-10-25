733f840f38853e02352296f2fde141f215e052a5
emelyarellano1 <emelyac91@hotmail.com>
2016-10-25 09:19:34 -0300

    modifique los getelemt y los querySelector porque estaba confundida

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// no se que hacer pero lo voy hacer
+// no se que hacer pero lo voy hacer fue dificil de inciar pero cunado lo hice no pare 
 function traductorIdioma(){
 	document.getElementById('form-signin-heading').innerHTML= "Por favor iniciar sesión";
-	document.queryselector('button').innerHTML= "iniciar Sesión";
-	document.getElementById('span').innerHTML= "Recordar Datos";
-	document.queryselector('inputPassword').placeholder= "Contraseña"
+	document.querySelector('button').innerHTML= "iniciar Sesión";
+	document.querySelector('span').innerHTML= "Recordar Datos";
+	document.getElementById('inputPassword').placeholder= "Contraseña";
 	document.getElementById('inputEmail').placeholder= "Correo Electrónico";
 }
