5ce9088b602930ccbc9d4073c6eb4d65aa76184b
emelyarellano1 <emelyac91@hotmail.com>
2016-10-24 23:01:38 -0300

    Cargando los cabios en la carpeta js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// no se que hacer pero lo voy hacer
+function traductorIdioma(){
+	document.getElementById('form-signin-heading').innerHTML= "Por favor iniciar sesión";
+	document.queryselector('button').innerHTML= "iniciar Sesión";
+	document.getElementById('span').innerHTML= "Recordar Datos";
+	document.queryselector('inputPassword').placeholder= "Contraseña"
+	document.getElementById('inputEmail').placeholder= "Correo Electrónico";
+}
